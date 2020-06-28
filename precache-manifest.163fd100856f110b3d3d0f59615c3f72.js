31897dc1acc5455abd6f8b72aaeb99605fc1235c
Amir <dev.amirhemmati1382@gmail.com>
2020-06-28 18:17:33 +0430

    Updates

---
+++ precache-manifest.163fd100856f110b3d3d0f59615c3f72.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cb144f6fdb01a5530c5159802ff4b72",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "2a863a32942d93649aa1",
+    "url": "/react-gh-pages/static/css/main.ad8d4ffc.chunk.css"
+  },
+  {
+    "revision": "8251975079f968f57672",
+    "url": "/react-gh-pages/static/js/2.76451be5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-gh-pages/static/js/2.76451be5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a863a32942d93649aa1",
+    "url": "/react-gh-pages/static/js/main.dd20dd21.chunk.js"
+  },
+  {
+    "revision": "02589ad42b183d27cf37",
+    "url": "/react-gh-pages/static/js/runtime-main.8904c2a9.js"
+  }
+]);
